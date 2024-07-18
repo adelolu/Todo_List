29e684ca4357bb796d3c9611286373426a554fd2
temiloluwa oyinloye <adelolu6@gmail.com>
2024-07-18 06:00:28 +0100

    database connected

--- index.js
+++ index.js
@@ -4,65 +4,106 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const ejs = require("ejs");
 const path = require("path");
-const { log } = require("console");
 app.set("view engine", "ejs");
-
+let errorMessage = "";
 // app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-// let tasks = ['a','b', 'c']
 let tasks = [];
 
-// const userSchema
-app.get("/", (req, res) => {
-  res.render("index", { task: tasks });
+const todoSchema = mongoose.Schema({
+  complete: { type: String, required: true },
+  task: { type: String, required: true },
+  content: { type: String, required: true },
 });
-app.get("/update/:index", (req, res) => {
-  let index = req.params.index;
-  let edittask = tasks[index];
-  res.render("edit", { task: edittask, index });
+const todomodel = mongoose.model("todo_collection", todoSchema);
+
+app.get("/", async (req, res) => {
+  let tasks = await todomodel.find({});
+  res.render("index", { task: tasks, message: errorMessage });
 });
 
-app.post("/upload", (req, res) => {
+app.get("/update/:id", async (req, res) => {
+  let { id } = req.params;
+  await todomodel
+    .findOne({ _id: id })
+    .then((edittask) => {
+      res.render("edit", { task: edittask, id });
+    })
+    .catch((error) => {
+      errorMessage = "There is an error";
+      res.redirect("/");
+    });
+});
+
+app.post("/upload", async (req, res) => {
   let { task, content } = req.body;
-  if (task != "") {
-    tasks.push({ complete: false, task: task, content: content });
+  let todo = { complete: false, task: task, content: content };
+  await todomodel
+    .create(todo)
+    .then((addtodo) => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      errorMessage = "There is an error";
+      res.redirect("/");
+    });
+});
+
+app.post("/checked/:id", async (req, res) => {
+  let { id } = req.params;
+
+  let update;
+  let task = await todomodel.findOne({ _id: id });
+  if (task.complete) {
+    update = false;
+  } else {
+    update = true;
   }
-  res.redirect("/");
+
+  await todomodel
+    .findByIdAndUpdate({ _id: id }, { complete: update })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      errorMessage = "There is an error";
+      res.redirect("/");
+    });
 });
 
-app.post("/checked/:index", (req, res) => {
-  let index = req.params.index;
-  tasks.forEach((element, i) => {
-    if (index == i && !element.complete) {
-      element.complete = true;
-    } else {
-      element.complete = false;
-    }
-  });
-  res.redirect("/");
+app.post("/delete/:id", async (req, res) => {
+  let { id } = req.params;
+  await todomodel
+    .deleteOne({ _id: id })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      errorMessage = "There is an error";
+      res.redirect("/");
+    });
 });
 
-app.post("/delete/:index", (req, res) => {
-  let index = req.params.index;
-  tasks.splice(index, 1);
-  res.redirect("/");
-});
-
-app.post("/edit/:index", (req, res) => {
-  let index = req.params.index;
+app.post("/edit/:id", async (req, res) => {
+  let { id } = req.params;
   let { task, content } = req.body;
-  console.log(req.body);
-  tasks[index].task = task;
-  tasks[index].content = content;
-  res.redirect("/");
+  await todomodel
+    .findByIdAndUpdate({ _id: id }, { task, content })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      errorMessage = "There is an error";
+      res.redirect("/");
+    });
 });
 
 port = 4000;
 app.listen(port, () => {
-  console.log("todo list started");
+  console.log("todo list started at " + port);
 });
 
 mongoose
@@ -73,5 +114,8 @@ mongoose
     }
   })
   .catch((err) => {
-    console.log(err);
+    {
+      errorMessage = "There is an error with connection to databse";
+      res.redirect("/");
+    }
   });
